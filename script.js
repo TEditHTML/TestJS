4b1d1e2eb27fe98c02887207e859d9c3f1ea2fad
TEdit <zablotsky1987@list.ru>
2021-02-25 00:50:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let arr = [];
+
+arr[0] = prompt("Как тебя зовут?", "");
+arr[1] = prompt("Фамилия?", "");
+arr[2] = +prompt("Возраст?", "18+");
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[2]);
+
+
+let oll = `toys`;
+console.log(`https://www.udemy.com/course/javascript_full/learn/lecture/18916196#overview/${oll}`);
+
+
+console.log(4 === "4");
