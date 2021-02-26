b9e37d8cd73a2f0ba0a7650866c22a3fbfb02dc7
TEdit <zablotsky1987@list.ru>
2021-02-26 18:47:52 +0300

    first test

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let arr = [];
+let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", ""),
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {
+        },
+        actors: {},
+        genres: [],
+        privat: false
+    },
+    lastFilm_1 = prompt("Один из последних просмотренных фильмов?", ""),
+    rangLastFilm_1 = +prompt("Насколько оцените его?", ""),
+    lastFilm_2 = prompt("Один из последних просмотренных фильмов?", ""),
+    rangLastFilm_2 = +prompt("Насколько оцените его?", "");
 
-arr[0] = prompt("Как тебя зовут?", "");
-arr[1] = prompt("Фамилия?", "");
-arr[2] = +prompt("Возраст?", "18+");
+personalMovieDB.movies[lastFilm_1] = rangLastFilm_1;
+personalMovieDB[movies][lastFilm_2] = rangLastFilm_2;
 
-console.log(arr);
-console.log(arr[0]);
-console.log(arr[2]);
-
-
-let oll = `toys`;
-console.log(`https://www.udemy.com/course/javascript_full/learn/lecture/18916196#overview/${oll}`);
-
-
-console.log(4 === "4");
+console.log(personalMovieDB);
