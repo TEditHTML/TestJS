17ffe0bee7f820862797b5621d374c44bae60157
TEdit <zablotsky1987@list.ru>
2023-10-30 23:44:21 +0300

    test massege

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Привет");
+let x = [];
+    x["name"] = "Ted",
+    x["age"] = 36;
+
+    console.log(x);
+    console.log(x.length);
+
+    x[0] = 10;
+    x[1] = 20;
+    console.log(x);
+    console.log(x.length);
+    console.log(x);
